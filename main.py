eabf736c9ecbeeddc154f93555b3962fc6afc868
Johnathan Davis <jdavis4329@gmail.com>
2022-07-19 17:32:26 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+champions = "We are the champions"
+friends = "my friends"
+fighting = "and we'll keep on fighting till the end"
+time = "No time for losers \'cause"
+world = "of the world"
+
+print(champions + ",", friends + ",\n" + fighting.capitalize() + ".\n" + champions + ",", champions.lower() + ".\n" + time, champions.lower(), world + ".")
